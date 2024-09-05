05426c46a870348873532b087f1d8d099cf0388c
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-09-05 18:28:29 +0330

    chore: remove .idea folder

--- server/src/db/migrate.ts
+++ server/src/db/migrate.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
+import * as process from "node:process";
 
 const migration = async () => {
   console.log("migration started...");
